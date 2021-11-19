36fbff3784ed446fb7272448a2cad4a80ae44dc1
jefferson1104 <jefferson1104junior@gmail.com>
2021-11-19 13:20:08 -0300

    instalando e configurando prettier  no projeto e com eslint

--- boilerplate/src/pages/index.tsx
+++ boilerplate/src/pages/index.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 type Props = {
-  title: string;
+  title: string
 }
 
-export default function Home({ title = 'React Avançado!' }: Props ) {
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
