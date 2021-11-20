600408f2bc8b26f306e1071ecfa7012889a70afc
jefferson1104 <jefferson1104junior@gmail.com>
2021-11-20 14:05:49 -0300

    configurando build do storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
   stories: ['../src/components/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-essentials',
-  ],
-  babel: async (options) => ({
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => {
+    return ({
     ...options,
-    plugins: [
-      ...options.plugins,
-      require.resolve('@babel/plugin-transform-react-jsx')
-    ]
-  }),
+    plugins: [...options.plugins, require.resolve('@babel/plugin-transform-react-jsx')]
+  })},
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Main from 'components/Main'
 
 export default function Home() {
-  const teste = 'bla'
   return <Main />
 }
