433e1c9af16c65c01f04ffb979fc4730f7536411
jefferson1104 <jefferson1104junior@gmail.com>
2021-11-23 11:37:34 -0300

    update boilerplate

--- src/components/Button/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Button from '.'
-
-export default {
-  title: 'Button',
-  component: Button
-} as Meta
-
-export const Basic: Story = (args) => <Button {...args} />
--- src/components/Logo/Logo.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Logo from '.'
-
-export default {
-  title: 'Logo',
-  component: Logo
-} as Meta
-
-export const Basic: Story = (args) => <Logo {...args} />
